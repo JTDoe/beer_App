a02b437e194a9001bac6ea39f19e14c14bc903d2
JTDoe <Jordendoerflinger@gmail.com>
2022-08-01 16:58:32 -0500

    like button added

--- src/beer.js
+++ src/beer.js
@@ -1,10 +1,12 @@
 import React from "react";
+import LikeButton from "./likeButton";
 
 export function Beer(props) {
   return (
     <li>
       <h3>{props.name}</h3>
       <img className="image" src={props.image} alt={props.name} />
+     <LikeButton />
     </li>
   );
 }
